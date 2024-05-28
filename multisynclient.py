a2d6b864290056bd3518f7e24776a62bb04d5dc4
Greg Baker <gregb@ifost.org.au>
2024-05-28 17:11:36 +1000

    Let's do client server, and protect the database that way.

---
+++ multisynclient.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import requests
+import ollama
+import time
+
+import sys
+parser = argparse.ArgumentParser()
+parser.add_argument("--server", required=True, help="Where the server is running")
+parser.add_argument("--congruent", type=int, help="Only process rows with ids that are congruent to this number")
+parser.add_argument("--modulo", type=int, help="Only process rows with ids that are congruent to --congruent modulo this number")
+parser.add_argument("--limit", type=int, help="Stop after processing this many rows")
+parser.add_argument("--progress-bar", action="store_true", help="Show a progress bar")
+parser.add_argument("--model", default="llama3", help="Which ollama model to use")
+parser.add_argument("--show-conversation", action="store_true", help="Show the prompt and output from the language model")
+args = parser.parse_args()
+
+def get_unresolved_words():
+    url = f'http://{args.server}:5000/unresolved'
+    params = {}
+    if args.congruent is not None and args.modulo is not None:
+        params['congruent'] = args.congruent
+        params['modulo'] = args.modulo
+    if args.limit is not None:
+        params['limit'] = args.limit
+    r = requests.get(url, params=params)
+    if r.status_code != 200:
+        sys.exit(f"{r.status_code} error from {args.server}: {r.text}")
+    for word in r.json():
+        yield word
+
+
+def get_sentence(sentence_id):
+    r = requests.get(f'http://{args.server}:5000/sentence', params={'sentence_id': sentence_id})
+    if r.status_code != 200:
+        sys.exit(f"{r.status_code} error from {args.server}: {r.text}")
+    return r.json()['sentence']
+
+def get_synsets(word_id):
+    r = requests.get(f'http://{args.server}:5000/synsets', params={'word_id': word_id})
+    if r.status_code != 200:
+        sys.exit(f"{r.status_code} error from {args.server}: {r.text}")    
+    return r.json()
+
+
+if args.progress_bar:
+    import tqdm
+    if args.limit:
+        iterator = tqdm.tqdm(get_unresolved_words(), total=args.limit)
+    else:
+        iterator = tqdm.tqdm(get_unresolved_words())
+else:
+    iterator = get_unresolved_words()
+
+
+for word_obj in iterator:
+    word_id = word_obj['word_id']
+    sentence_id = word_obj['sentence_id']
+    word_number = word_obj['word_number']
+    word = word_obj['word']
+    sentence = get_sentence(sentence_id)
+
+    prompt = f"""Consider this sentence:
+    {sentence}
+The word `{word}` (which is word #{word_number+1}) can have multiple meanings. Which of the following meanings is it being used for in this sentence?
+
+"""
+    for synset in get_synsets(word_id):
+        synset_id = synset['synset_id']
+        description = synset['description']
+        prompt += f" ({synset_id}) -- {description}"
+        if 'example' in synset and synset['example'].strip() != '':
+            prompt += f"\n       Example use: {synset['example']}"
+        prompt += "\n\n"
+    prompt += """ (other) -- none of those synsets match the word's meaning here
+
+
+Answer in JSON format, with a key of "synset", e.g.
+
+{
+    "synset": "foo.n.01"
+}
+    """
+
+    if args.show_conversation:
+        sys.stderr.write("-" * 70 + "\n")
+        sys.stderr.write(time.asctime())
+        sys.stderr.write(prompt)
+        sys.stderr.write("\n")
+    starting_moment = time.time()
+    response = ollama.chat(model=args.model, messages=[
+        {
+            'role': 'user',
+            'content': prompt
+        }
+        ],format='json', stream=True)
+    so_far = ""
+    for chunk in response:
+        if args.show_conversation:
+            sys.stderr.write(chunk['message']['content'])
+            sys.stderr.flush()
+        so_far += chunk['message']['content']
+        try:
+            answer = json.loads(so_far)
+            # if that worked...
+            break
+        except:
+            pass
+    compute_time = time.time() - starting_moment
+
+    r = requests.post(f'http://{args.server}:5000/update',
+                      json={'word_id': word_id,
+                            'resolved_synset': answer['synset'],
+                            'compute_time': compute_time,
+                            'model': args.model })
---
+++ multisynserver.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sqlite3
+import time
+
+import sys
+parser = argparse.ArgumentParser()
+parser.add_argument("--database", required=True, help="Where the database is")
+args = parser.parse_args()
+
+conn = sqlite3.connect(args.database)
+cursor = conn.cursor()
+cursor.execute("pragma busy_timeout = 30000;")
+cursor.execute("pragma journal_mode = WAL;")
+
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/unresolved', methods=['GET'])
+def unresolved():
+    congruent = request.args.get('congruent', default=None, type=int)
+    modulo = request.args.get('modulo', default=None, type=int)
+    limit = request.args.get('limit', default=None, type=int)
+    query = "select id, sentence_id, word_number, word from words where resolved_synset is null"
+    if congruent is not None and modulo is not None:
+        cursor.execute(f"create index if not exists unresolved_words_{congruent}_mod_{modulo} on words(id) where resolved_synset is null and id % {modulo} = {congruent}")
+        query += f" and id % {modulo} = {congruent}"
+    else:
+        cursor.execute(f"create index if not exists unresolved_words on words(resolved_synset) where resolved_synset is null")
+    if limit is not None:
+        query += f" limit {limit}"
+
+    cursor.execute(query)
+    answer = []
+    # next two lines should probably fetchall
+    for (word_id, sentence_id, word_number, word) in cursor:
+        answer.append({'word_id': word_id,
+                       'sentence_id': sentence_id,
+                       'word_number': word_number,
+                       'word': word})
+    return jsonify(answer)
+
+@app.route('/sentence', methods=['GET'])
+def sentence():
+    sentence_id = request.args.get('sentence_id', default=None, type=int)
+    
+    if sentence_id is None:
+        return jsonify({'error': 'Sentence ID is required'}), 400
+    cursor.execute("select sentence from sentences where sentences.id = ?", [sentence_id])
+    row = cursor.fetchone()
+    if row is None:
+        return jsonify({'error': 'Sentence ID not found'}), 404
+    sentence = row[0]
+    result = {'sentence': sentence }
+    return jsonify(result)
+
+@app.route('/synsets', methods=['GET'])
+def synsets():
+    word_id = request.args.get('word_id', default=None, type=int)
+    
+    if word_id is None:
+        return jsonify({'error': 'Word ID is required'}), 400
+
+    cursor.execute("select synsets.id, description, examples from synsets join word_synsets on (synsets.id = synset_id) and word_id = ?", [word_id])
+    answer = []
+    for (synset_id, description, example) in cursor:
+        answer.append({'synset_id': synset_id,
+                       'description': description})
+        if example is not None:
+            answer[-1]['example'] = example
+    return jsonify(answer)
+
+@app.route('/update', methods=['POST'])
+def update():
+    data = request.json
+    
+    if 'word_id' not in data or 'resolved_synset' not in data or 'compute_time' not in data:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    word_id = data['word_id']
+    resolved_synset = data['resolved_synset']
+    compute_time = data['compute_time']
+    model = data['model']
+
+    cursor.execute("update words set resolved_synset = ?, resolving_model=?, resolved_timestamp = current_timestamp, resolution_compute_time=? where id = ?", resolved_synset, model, compute_time, word_id)
+    conn.commit()
+    
+    result = {'message': 'done'}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(threaded=False, processes=1, host='0.0.0.0')
+
+
