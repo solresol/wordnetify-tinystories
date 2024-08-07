0247ab637f280bc38feb6e86c346e4429623b9ba
Greg Baker <gregb@ifost.org.au>
2024-08-07 18:27:20 +1000

    Report on costs.

--- batchfetch.py
+++ batchfetch.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--database", required=True, help="Where the database is")
 parser.add_argument("--openai-api-key", default=os.path.expanduser("~/.openai.key"))
 parser.add_argument("--progress-bar", action='store_true', help="Show a progress bar for updating the database on each batch")
+parser.add_argument("--report-costs", action="store_true", help="Report the cost of the runs fetched")
 args = parser.parse_args()
 
 api_key = open(args.openai_api_key).read().strip()
@@ -27,6 +28,9 @@ update_cursor.execute("pragma busy_timeout = 30000;")
 update_cursor.execute("pragma journal_mode = WAL;")
 
 cursor.execute("select id, openai_batch_id from batches where when_sent is not null and when_retrieved is null")
+
+total_prompt_tokens = 0
+total_completion_tokens = 0
 
 update_cursor.execute("begin transaction")
 work_to_be_done = False
@@ -54,5 +58,15 @@ for local_batch_id, openai_batch_id in cursor:
         update_cursor.execute("update words set resolved_synset = ?, resolving_model=?, resolved_timestamp = current_timestamp where id = ?", [arguments['synset'], model, word_id])
         update_cursor.execute("insert into costs (word_id, prompt_tokens, completion_tokens) values (?,?,?)",
                               [word_id, usage['prompt_tokens'], usage['completion_tokens']])
+        total_prompt_tokens += usage['prompt_tokens']
+        total_completion_tokens += usage['completion_tokens']
     update_cursor.execute("update batches set when_retrieved = current_timestamp where id = ?", [local_batch_id])
 conn.commit()
+
+if args.report_costs:
+    print(f"Prompt tokens:     {total_prompt_tokens}")
+    print(f"Completion tokens: {total_completion_tokens}")
+    prompt_pricing = 0.075 / 1000000
+    completion_pricing = 0.3 / 1000000
+    cost = prompt_pricing * total_prompt_tokens + completion_pricing * total_completion_tokens
+    print(f"Cost (USD):        {cost:.2f}")
