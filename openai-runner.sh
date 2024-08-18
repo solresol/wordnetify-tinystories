e2f362064d0855cb2241d2e67df3c256279f27ca
Greg Baker <gregb@ifost.org.au>
2024-08-18 18:25:38 +1000

    How I've been running it.

---
+++ openai-runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /tinystories/wordnetify-tinystories
+. .venv/bin/activate
+
+python3 generate_multisynset_batch.py --database TinyStories.sqlite \
+	 --congruent 3 --modulo 1000 \
+	 --output-file .batchfiles/batch-$(date +%F-%T).jsonl --limit 40000 --progress-bar \
+	 --batch-id-save-file .batchid.txt && \
+python3 batchcheck.py --database TinyStories.sqlite  --only-batch $(< .batchid.txt) --monitor && \
+python3 batchfetch.py --database TinyStories.sqlite --progress-bar --report-costs
